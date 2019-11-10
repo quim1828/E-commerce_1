86899f98abfe57a235e1c058984e9707d099fab5
CiceroRegis <ciceroregis25@gmail.com>
2019-11-10 16:58:56 -0300

    client registration

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import entities.Client;
+import entities.Order;
 import entities.Product;
 
 public class Program {
@@ -9,11 +10,16 @@ public class Program {
 
 	Product product = new Product(null, 0);
 	Client client =  new Client();
+	Order order =  new Order();
+	client.setName("Tiringa");
+	client.setCpf("0000000000");
 	
 	product.setName("Feijão");
 	product.setPrice(20.00);
 	
-	System.out.println("Nome do produto: " + product.getName() + "\n" + "Preço do produto: " +  product.getPrice());
+	order.setClient(client);
+	System.out.println("Nome do produto: " + product.getName() + "\n" + "Preço do produto: " +  product.getPrice() + "\n"+
+			"order:" + order.getClient());
 	}
 
 }
